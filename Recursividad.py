defc03cdfb37fbfc0b286a73c02ac2b9a8a9d3d0
CrisBarreraml <cristopherbarrera789@gmail.com>
2025-02-24 23:48:38 -0600

    Recursividad.py

--- Recursividad.py
+++ Recursividad.py
@@ -1,10 +1,11 @@
-def invertir_cadena(s):
-    if len(s) == 0:
-        return s
-    else:
-        return s[-1] + invertir_cadena(s[:-1])
+def contar_digitos(n):
+    if n == 0:
+        return 0
+    return 1 + contar_digitos(n // 10)
 
-cadena = input("Ingresa una cadena: ")
-cadena_invertida = invertir_cadena(cadena)
-print(f"Cadena invertida: {cadena_invertida}")
+numero = int(input("Ingresa un número entero positivo: "))
+if numero < 0:
+    print("Por favor, ingresa un número positivo.")
+else:
+    print(f"El número {numero} tiene {contar_digitos(numero)} dígitos.")
 
